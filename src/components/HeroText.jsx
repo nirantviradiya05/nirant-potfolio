8e56a0c7f71e81b29a88a6828919c52281131e8e
Nirant <nirant05viradiya@gmail.com>
2025-09-11 15:40:55 +0530

    Updated portfolio

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Nirant
         </motion.p>
         <div>
           <motion.p
